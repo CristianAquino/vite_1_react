6670221a39af2f02bfadcd581995a83dbd81f252
cristianaquino <ycristian10@gmail.com>
2022-10-22 20:39:08 -0500

    delete

--- react-vite/src/trash/components/Logout.jsx
+++
@@ -1,8 +0,0 @@
-export const Logout = ({ handleLogout, user }) => {
-  return (
-    <div>
-      <p>Bienvenido {user.username}</p>
-      <button onClick={handleLogout}>Logout</button>
-    </div>
-  );
-};
--- react-vite/src/trash/routes/Home.jsx
+++
@@ -1,7 +0,0 @@
-import { useStore } from "../store/StoreProvider";
-
-export const Home = () => {
-  const { user } = useStore();
-
-  return <h1>Home {user.name}</h1>;
-};
