607964448d73e1a1438992ec32364aacb2b9afd3
cristianaquino <ycristian10@gmail.com>
2022-10-25 17:07:26 -0500

    ProtectedRoute

---
+++ app_react_vite/src/App.jsx
@@ -0,0 +1,40 @@
+import "./App.css";
+
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import { Layout } from "./pages/Layout";
+import { About } from "./pages/About";
+import { Login } from "./components/Login/Login";
+import { Register } from "./components/Register/Register";
+import { Home } from "./pages/Home";
+import { ProfileUser } from "./components/Profile/ProfileUser";
+
+import { ProtectedRoute } from "./routes/ProtectedRoute";
+import useAuth from "./hooks/useAuth";
+
+const App = () => {
+  const { isLogged } = useAuth();
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route element={<ProtectedRoute isLogged={!isLogged} />}>
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+        <Route
+          path="profile"
+          element={
+            <ProtectedRoute isLogged={isLogged}>
+              <ProfileUser />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="*" element={<Navigate replace to={"/"} />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default App;
