d9a3f8f425e02f0b382a8be68b8f0207e130bed8
cristianaquino <ycristian10@gmail.com>
2022-11-03 23:06:19 -0500

    home1

---
+++ app_react_vite/src/components/Home1/Home1.jsx
@@ -0,0 +1,12 @@
+import { LayoutCard } from "../LayoutCard/LayoutCard";
+import { Ranking } from "../Ranking/Ranking";
+import "./Home1.css";
+
+export const Home1 = () => {
+  return (
+    <div className="home1">
+      <LayoutCard />
+      <Ranking />
+    </div>
+  );
+};
