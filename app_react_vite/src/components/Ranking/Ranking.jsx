b47a50d30ecab064bb4d0d9ca396be56e78a59a9
cristianaquino <ycristian10@gmail.com>
2022-11-08 16:25:55 -0500

    ranking

--- app_react_vite/src/components/Ranking/Ranking.jsx
+++ app_react_vite/src/components/Ranking/Ranking.jsx
@@ -1,9 +1,21 @@
+import { Link } from "react-router-dom";
 import { Rank } from "../Rank/Rank";
 import "./Ranking.css";
 
 export const Ranking = () => {
   return (
     <div className="ranking">
+      <ul className="head_tags">
+        <li className="active">
+          <Link>Top Day</Link>
+        </li>
+        <li>
+          <Link>Top Week</Link>
+        </li>
+        <li>
+          <Link>Top Month</Link>
+        </li>
+      </ul>
       <Rank />
       <Rank />
       <Rank />
