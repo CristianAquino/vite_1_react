ec6d65739b58efa9631f30d52bfdf6c27dbacd97
cristianaquino <ycristian10@gmail.com>
2022-11-08 16:27:47 -0500

    authProvider

--- app_react_vite/src/provider/AuthProvider.jsx
+++ app_react_vite/src/provider/AuthProvider.jsx
@@ -3,12 +3,13 @@ import { createContext, useState } from "react";
 export const authContext = createContext();
 
 const AuthProvider = ({ children }) => {
+  const [profile, setProfile] = useState(null);
   const [token, setToken] = useState(() =>
     window.localStorage.getItem("token")
   );
 
   return (
-    <authContext.Provider value={{ token, setToken }}>
+    <authContext.Provider value={{ profile, token, setToken, setProfile }}>
       {children}
     </authContext.Provider>
   );
